c41f88e71939d7faf3b4e687fe31209c88232507
Sanusi-Moshood <snsmoshood@gmail.com>
2022-09-09 21:06:11 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -mainn
+ *
+ * Return: return 0 (Successful)
+ */
+int main(void)
+{
+	for (int x = 'A'; x <= 'z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	return(0);
+}
